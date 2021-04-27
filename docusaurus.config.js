dda8b492dbff76bd939334fdf715439074d67ad5
GINAGC <70196799+GINAGC@users.noreply.github.com>
2021-04-27 16:01:16 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Documentation",
   tagline: "",
   url: "https://k8-proxy.github.io",
-  baseUrl: "/EAP-documentation/",
+  baseUrl: "/k8-proxy-documentation/",
   favicon: "img/favicon.ico",
   organizationName: "k8-proxy", // Usually your GitHub org/user name.
   projectName: "k8-proxy-documentation", // Usually your repo name.
